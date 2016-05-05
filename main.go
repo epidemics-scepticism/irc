d7f4da4d647c8b913a515d12c4b2997daeec48e6
epidemics-scepticism <cacahuatl@autistici.org>
2016-05-05 17:18:48 +0000

    Update main.go

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ var (
 	IrcNick   = flag.String("nick", generateNick(), "Nick to use on IRC")
 	ircProxy  = flag.String("proxy", "127.0.0.1:9050", "SOCKS5 proxy as host:port")
 	ircTls    = flag.Bool("tls", true, "use TLS")
+	ircClean  = flag.Bool("clean", true, "Strip join/part/quit/notice")
 )
 
 func main() {
